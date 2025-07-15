71799145092b0b80361690bb068fc8d4c30c618d
Tim Boex <timboex@gmail.com>
2025-07-15 14:52:37 -0500

    fixed settings names

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -5,8 +5,8 @@ import logging
 PACKAGE_SEVERITY_LEVELS = {
     'uvicorn': logging.DEBUG
 }
-SEVERITY_LEVEL = os.getenv('REDIS_HOST', 'DEBUG')
-LOGGER_NAME = os.getenv('REDIS_HOST', 'kmig')
+SEVERITY_LEVEL = os.getenv('SECURITY_LEVEL', 'DEBUG')
+LOGGER_NAME = os.getenv('LOGGER_NAME', 'kmig')
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
